4718c06c1a0a3223d8948b50db2f211d7d353744
Evansmuscle <Evansmuscle@users.noreply.github.com>
2021-07-18 14:59:35 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.d505b9f4f980fa85e78a7e4734761090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9bffb9b6a7baea5d1eedf1c37270aa",
+    "url": "https://Evansmuscle.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8ff304e6d7a3e33c2438",
+    "url": "https://Evansmuscle.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c20dc0119ca65afef13f",
+    "url": "https://Evansmuscle.github.io/task-management-frontend/static/js/2.0c3a1f25.chunk.js"
+  },
+  {
+    "revision": "8ff304e6d7a3e33c2438",
+    "url": "https://Evansmuscle.github.io/task-management-frontend/static/js/main.d4add7e1.chunk.js"
+  },
+  {
+    "revision": "a9149339617d4a76342e",
+    "url": "https://Evansmuscle.github.io/task-management-frontend/static/js/runtime~main.f4656322.js"
+  }
+]);
